c3a788752d49a8412aa0a445c7234d7b782fc536
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-06-25 09:51:14 -0500

    Ran pre-commit

--- src/RunPlots/top10UndirectedNodes.py
+++ src/RunPlots/top10UndirectedNodes.py
@@ -1,31 +1,42 @@
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 import json
 
-def main()-> None:
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 
-    with open('/Users/karolinaryzka/Documents/neo4jQueries/Data/top10AllDegrees.json', 'r', encoding='utf-8-sig') as file:
+
+def main() -> None:
+    with open(
+        "/Users/karolinaryzka/Documents/neo4jQueries/Data/top10AllDegrees.json",
+        "r",
+        encoding="utf-8-sig",
+    ) as file:
         data = json.load(file)
 
-    extractedData = [{'oa_id': item['n']['properties']['oa_id'], 'degree': item['degree']} for item in data]
+    extractedData = [
+        {"oa_id": item["n"]["properties"]["oa_id"], "degree": item["degree"]}
+        for item in data
+    ]
     df = pd.DataFrame(extractedData)
 
     # Plot the bar chart
     sns.set_theme()
-    graph = sns.barplot(data=df, x='oa_id', y='degree')
-    
-    graph.set_xlabel('OPEN ALEX ID')
-    graph.set_ylabel('Number of Citations and References')
-    graph.set_title('Top 10 Influential Nodes in Graph (un-directed)')
-    plt.xticks(rotation=45, ha='right')  # Rotate x-axis labels for better readability
-    #label bars
-    for index, value in enumerate(df['degree']):
-        graph.text(index, value, str(value), ha='center', va='bottom')
+    graph = sns.barplot(data=df, x="oa_id", y="degree")
+
+    graph.set_xlabel("OPEN ALEX ID")
+    graph.set_ylabel("Number of Citations and References")
+    graph.set_title("Top 10 Influential Nodes in Graph (un-directed)")
+    plt.xticks(rotation=45, ha="right")  # Rotate x-axis labels for better readability
+    # label bars
+    for index, value in enumerate(df["degree"]):
+        graph.text(index, value, str(value), ha="center", va="bottom")
 
     # Display the plot
     plt.tight_layout()
-    plt.savefig("/Users/karolinaryzka/Documents/neo4jQueries/Visualizations/top10UndirectedNodes.png")
+    plt.savefig(
+        "/Users/karolinaryzka/Documents/neo4jQueries/Visualizations/top10UndirectedNodes.png"
+    )
+
 
 if __name__ == "__main__":
     main()
--- src/sql.py
+++ src/sql.py
@@ -1,19 +1,23 @@
-import pandas as pd 
 import sqlite3
-from typing import Optional
-from tqdm import tqdm
-import seaborn as sns
-import matplotlib.pyplot as plt
 from sqlite3 import Connection, Cursor
+from typing import Iterator, Optional, Union
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 from pandas import DataFrame, Series
-from typing import Iterator, Union
+from tqdm import tqdm
 
 
-def loadTableIntoDataframe(conn: Connection, tableName: str, columns: Optional[list] = None, returnIterator: bool = False) -> pd.DataFrame:
-    
+def loadTableIntoDataframe(
+    conn: Connection,
+    tableName: str,
+    columns: Optional[list] = None,
+    returnIterator: bool = False,
+) -> pd.DataFrame:
     columns_str = "*" if columns is None else ", ".join(columns)
     query = f"SELECT {columns_str} FROM {tableName}"
-    
+
     # Execute the query and read the result into a pandas DataFrame
     if returnIterator:
         df = pd.read_sql_query(query, conn, chunksize=50000)
@@ -21,32 +25,34 @@ def loadTableIntoDataframe(conn: Connection, tableName: str, columns: Optional[l
         df = pd.read_sql_query(query, conn)
     return df
 
+
 def countNodes(conn: Connection) -> int:
     query = f"SELECT COUNT(oaid) FROM works"
     result: Cursor = conn.execute(query)
     return result.fetchone()[0]
-    
+
+
 def countRelationships(conn: Connection) -> int:
     query = f"SELECT COUNT(*) AS numberOfRelationships FROM relationship_cites"
     result: Cursor = conn.execute(query)
     return result.fetchone()[0]
 
+
 def plotNodesVsRelationships(conn: Connection) -> None:
     nodeCount = countNodes(conn)
     relationshipCount = countRelationships(conn)
-    data = {
-    '': ['Nodes', 'Relationships'],
-    'Count': [nodeCount, relationshipCount]
-}
+    data = {"": ["Nodes", "Relationships"], "Count": [nodeCount, relationshipCount]}
     # Convert data to a pandas DataFrame
     df = pd.DataFrame(data)
     # Plotting using seaborn
     plt.figure(figsize=(8, 6))
-    sns.barplot(data=df, x='', y='Count')
-    plt.ylabel('Count')
-    plt.title('Node Count vs Relationship Count')
+    sns.barplot(data=df, x="", y="Count")
+    plt.ylabel("Count")
+    plt.title("Node Count vs Relationship Count")
     plt.tight_layout()
-    plt.savefig("/Users/karolinaryzka/Documents/neo4jQueries/src/nodesVsRelationships.png")
+    plt.savefig(
+        "/Users/karolinaryzka/Documents/neo4jQueries/src/nodesVsRelationships.png"
+    )
 
 
 def graphDensity(conn: Connection) -> float:
@@ -71,11 +77,11 @@ def inDegreeCentrality(conn: Connection) -> pd.DataFrame:
     query = f"SELECT ref_oaid, Count(*) as c FROM relationship_cites GROUP BY ref_oaid HAVING Count(*) > 15 ORDER BY c DESC LIMIT 15;"
     result = pd.read_sql_query(query, conn)
     plt.figure(figsize=(12, 6))
-    sns.barplot(data=result, x = 'ref_oaid', y = 'c')
-    plt.xlabel('OPEN ALEX ID')
-    plt.ylabel('In-Degree Centrality')
-    plt.title('Top 15 Most Cited Papers')
-    plt.xticks(rotation=45, ha='right')
+    sns.barplot(data=result, x="ref_oaid", y="c")
+    plt.xlabel("OPEN ALEX ID")
+    plt.ylabel("In-Degree Centrality")
+    plt.title("Top 15 Most Cited Papers")
+    plt.xticks(rotation=45, ha="right")
     plt.tight_layout()
     plt.savefig("/Users/karolinaryzka/Documents/neo4jQueries/src/inDegree.png")
     return result
@@ -85,11 +91,11 @@ def outDegreeCentrality(conn: Connection) -> pd.DataFrame:
     query = f"SELECT work_oaid, Count(*) as c FROM relationship_cites GROUP BY work_oaid HAVING Count(*) > 15 ORDER BY c DESC LIMIT 15;"
     result = pd.read_sql_query(query, conn)
     plt.figure(figsize=(12, 6))
-    sns.barplot(data=result, x='work_oaid', y='c')
-    plt.xlabel('OPEN ALEX ID')
-    plt.ylabel('Out-Degree Centrality')
-    plt.title('Top 15 Papers that Reference Others')
-    plt.xticks(rotation=45, ha='right')
+    sns.barplot(data=result, x="work_oaid", y="c")
+    plt.xlabel("OPEN ALEX ID")
+    plt.ylabel("Out-Degree Centrality")
+    plt.title("Top 15 Papers that Reference Others")
+    plt.xticks(rotation=45, ha="right")
     plt.tight_layout()
     plt.savefig("/Users/karolinaryzka/Documents/neo4jQueries/src/outDegree.png")
     return result
@@ -120,13 +126,15 @@ def mostFrequentDegreeCounts(conn: Connection) -> pd.DataFrame:
     """
     result = pd.read_sql_query(query, conn)
     plt.figure(figsize=(12, 6))
-    sns.barplot(data=result, x='degree_count', y='frequency')
-    plt.xlabel('Degree')
-    plt.ylabel('Frequency')
-    plt.title('Top 15 Most Frequent Number of Citations')
-    plt.xticks(rotation=45, ha='right')
+    sns.barplot(data=result, x="degree_count", y="frequency")
+    plt.xlabel("Degree")
+    plt.ylabel("Frequency")
+    plt.title("Top 15 Most Frequent Number of Citations")
+    plt.xticks(rotation=45, ha="right")
     plt.tight_layout()
-    plt.savefig("/Users/karolinaryzka/Documents/neo4jQueries/src/mostFrequentDegrees.png")
+    plt.savefig(
+        "/Users/karolinaryzka/Documents/neo4jQueries/src/mostFrequentDegrees.png"
+    )
     return result
 
 
@@ -134,6 +142,7 @@ def nodesOfDegreeX(conn: Connection, degreeCount: int) -> pd.DataFrame:
     query = f"SELECT ref_oaid FROM relationship_cites GROUP BY ref_oaid HAVING COUNT(*) = {degreeCount};"
     result = pd.read_sql_query(query, conn)
     return result
+
 
 def mostConnectedNodes(conn: Connection) -> pd.DataFrame:
     query = f"""
@@ -153,18 +162,19 @@ def mostConnectedNodes(conn: Connection) -> pd.DataFrame:
     """
     result = pd.read_sql_query(query, conn)
     plt.figure(figsize=(12, 6))
-    sns.barplot(data=result, x='node_id', y='total_degree')
-    plt.xlabel('OPEN ALEX ID')
-    plt.ylabel('Total Degrees (in both directions)')
-    plt.title('Top 15 Most Connected Papers')
-    plt.xticks(rotation=45, ha='right')
+    sns.barplot(data=result, x="node_id", y="total_degree")
+    plt.xlabel("OPEN ALEX ID")
+    plt.ylabel("Total Degrees (in both directions)")
+    plt.title("Top 15 Most Connected Papers")
+    plt.xticks(rotation=45, ha="right")
     plt.tight_layout()
-    plt.savefig("/Users/karolinaryzka/Documents/neo4jQueries/src/mostConnectedNodes.png")
+    plt.savefig(
+        "/Users/karolinaryzka/Documents/neo4jQueries/src/mostConnectedNodes.png"
+    )
     return result
 
 
-def main(  
-) -> None:
+def main() -> None:
     conn = sqlite3.connect("/Users/karolinaryzka/Documents/neo4jQueries/works_cites.db")
     print(f"Number of nodes: {countNodes(conn)}")
     print(f"Number of relationships: {countRelationships(conn)}")
@@ -178,7 +188,6 @@ def main(
     print(f"Nodes with Degree of 10: {nodesOfDegreeX(conn, 10)}")
     print(f"Most Connected Nodes (in both directions): {mostConnectedNodes(conn)}")
     conn.close()
-    
 
 
 if __name__ == "__main__":
